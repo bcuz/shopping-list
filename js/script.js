660bd6c3680003aa0cad8522f1c00b1c2a9e8562
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-05 15:13:45 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(function() {
 
+  // prefer not to have to do this two times
   $(".btn").click(function() {
     console.log("hi");
   });
 
     $('.item').keypress(function(e){
-        if(e.which == 13){//Enter key pressed
+        if(e.which == 13){
             console.log("bz");
         }
     });
