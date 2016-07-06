dc07316a1aa9d33fe02d1315a7ca18b23c8f64c6
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-06 06:47:39 -0400

    gona make it submit from input later

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 $(function() {
 
-  // prefer not to have to do this two times
   $(".btn").click(function() {
     console.log("hi");
   });
-
-    $('.item').keypress(function(e){
-        if(e.which == 13){
-            console.log("bz");
-        }
-    });
 
 });
