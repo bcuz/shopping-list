039cbbf567e712f18c8ce5d8d48fcc935edf314b
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-06 07:52:21 -0400

    remove list item if del btn clicked

--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@ $(function() {
     if ($(".item").val().length === 0 ) {
       alert("Enter something yo")
     } else {
-      $(".list").append('<input type="checkbox" value="hi">' + $(".item").val() + '</input><a href="#">Delete</a><br>')
+      $(".list").append('<div><input type="checkbox" value="hi">' + $(".item").val() + '</input><a href="#">Delete</a><br></div>')
 
     }
 
     $(".item").val("")
 
   });
+
+  $(".list").on("click", "a", function() {
+    $(this).parent().remove()
+  })
 
 
 
