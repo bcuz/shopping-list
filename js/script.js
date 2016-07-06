e584a5addd69c7a70489750b972ede79e5946fdb
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-06 07:43:31 -0400

    delte buttn

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(function() {
     if ($(".item").val().length === 0 ) {
       alert("Enter something yo")
     } else {
-      $(".list").append('<input type="checkbox" value="hi">' + $(".item").val() + '</input><br>')
+      $(".list").append('<input type="checkbox" value="hi">' + $(".item").val() + '</input><a href="#">Delete</a><br>')
 
     }
 
