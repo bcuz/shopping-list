001325b80f3ff45cca9bba6c499c8d82967b40d2
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-07 08:32:21 -0400

    add missing semicolons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 $(function() {
   var item_added = $("input[type='text']");
 
-  item_added.val("")
+  item_added.val("");
 
   $(".btn").click(function() {
     if (item_added.val().length === 0 ) {
-      alert("Enter something yo")
+      alert("Enter something yo");
     } else {
       $(".shopping-list").append('\
         <div>\
           <input type="checkbox">\
           <label contenteditable="true">' + item_added.val() + '</label>\
           <a href="#">Delete</a><br>\
-        </div>')
+        </div>');
     }
 
-    item_added.val("")
+    item_added.val("");
 
   });
 
@@ -26,13 +26,13 @@ $(function() {
 });
 
   $(".shopping-list").on("click", "a", function() {
-    $(this).parent().remove()
+    $(this).parent().remove();
   })
   .on("click", "input[type='checkbox']", function() {
   if($(this).is(":checked")) {
-    $(this).next().css("text-decoration", "line-through")
+    $(this).next().css("text-decoration", "line-through");
   } else {
-    $(this).next().css("text-decoration", "none")
+    $(this).next().css("text-decoration", "none");
   }
-  })
+  });
 });
