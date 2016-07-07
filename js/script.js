4d81c7600b4ed407c43ffb13793d611ccef52e10
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-07 07:29:44 -0400

    clean up code

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(function() {
     if ($(".item").val().length === 0 ) {
       alert("Enter something yo")
     } else {
-      $(".list").append('<div><input type="checkbox" value="hi"><label contenteditable="true">' + $(".item").val() + '</label><a href="#">Delete</a><br></div>')
+      $(".list").append('<div><input type="checkbox"><label contenteditable="true">' + $(".item").val() + '</label><a href="#">Delete</a><br></div>')
 
     }
 
